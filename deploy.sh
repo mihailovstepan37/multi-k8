067e38fa658b710f77a04d4a07356e289365fe38
Dmitriy <dimarakov21@gmail.com>
2020-08-13 22:27:40 +0300

    change repo

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,15 @@
-docker build -t stephengrider/multi-client:latest -t stephengrider/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server:latest -t stephengrider/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker:latest -t stephengrider/multi-worker:$SHA -f ./worker/Dockerfile ./worker
-docker push -t stephengrider/multi-client:latest
-docker push -t stephengrider/multi-server:latest
-docker push -t stephengrider/multi-worker:latest
+docker build -t diesel457/multi-client:latest -t diesel457/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t diesel457/multi-server:latest -t diesel457/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t diesel457/multi-worker:latest -t diesel457/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push -t diesel457/multi-client:latest
+docker push -t diesel457/multi-server:latest
+docker push -t diesel457/multi-worker:latest
 
-docker push -t stephengrider/multi-client:$SHA
-docker push -t stephengrider/multi-server:$SHA
-docker push -t stephengrider/multi-worker:$SHA
+docker push -t diesel457/multi-client:$SHA
+docker push -t diesel457/multi-server:$SHA
+docker push -t diesel457/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=stephengrider/multi-server:$SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client:$SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker:$SHA
+kubectl set image deployments/server-deployment server=diesel457/multi-server:$SHA
+kubectl set image deployments/client-deployment client=diesel457/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=diesel457/multi-worker:$SHA
